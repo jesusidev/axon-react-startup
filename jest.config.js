24f66680f36781aac791ab449577868bad986b6e
Jesus G <jesus@guzman1.com>
2020-04-09 21:06:41 -0700

    Added in jest coverage and updated packages

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,19 @@
 module.exports = {
   roots: ['<rootDir>/src'],
   transform: {
-    '\\.(ts|tsx)?$': 'babel-jest'
+    '\\.(ts|tsx)?$': 'babel-jest',
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-  testPathIgnorePatterns: ['/node_modules/', '/public/']
+  testPathIgnorePatterns: ['/node_modules/', '/public/'],
+  collectCoverage: true,
+  coverageReporters: ['lcov'],
+  coverageDirectory: 'coverage',
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80,
+    },
+  },
 };
