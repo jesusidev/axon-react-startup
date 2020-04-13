f970f876ac7b5f1e791f1814a962a060fd21fa8f
Jesus G <jesus@guzman1.com>
2020-04-12 20:25:29 -0700

    style(CSS Reset): Used TailWind CSS Reset Base

--- src/app.tsx
+++ src/app.tsx
@@ -1,31 +1,14 @@
 import React from 'react';
-import Button from './components/button/Button';
-import helloString from './helpers/hello';
+import { GlobalStyle } from './styles/default';
 
 const App: React.FC = () => {
-  const [name, setName] = React.useState<string | null>('');
-  const [isLoading, setIsLoading] = React.useState(false);
-
-  const onButtonClick = (): void => {
-    setIsLoading(true);
-    setTimeout(() => {
-      setIsLoading(false);
-      setName(`🎈${name}🎈`);
-    }, 2000);
-  };
-
   return (
-    <div className="wrapper">
-      <h1>{helloString(name)}</h1>
-      <input
-        type="text"
-        placeholder="Enter your name"
-        onChange={(e) => setName(e.target.value)}
-      />
-      <Button disabled={isLoading} onClick={onButtonClick}>
-        {isLoading ? 'Loading...' : 'Click for balloons'}
-      </Button>
-    </div>
+    <>
+      <GlobalStyle />
+      <div className="wrapper">
+        <h1>Hello React</h1>
+      </div>
+    </>
   );
 };
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,4 @@
+import 'focus-visible';
 import ReactDOM from 'react-dom';
 import React from 'react';
 import App from './app';
