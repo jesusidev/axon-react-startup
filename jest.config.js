9e92a7f23187009ef61298025c633454eb07e127
J <jesus@axonbridge.com>
2020-05-18 21:04:02 -0700

    jest config

--- jest.config.js
+++ jest.config.js
@@ -1,19 +1,34 @@
 module.exports = {
-  roots: ['<rootDir>/src'],
+  preset: 'ts-jest/presets/js-with-babel',
   transform: {
-    '\\.(ts|tsx)?$': 'babel-jest',
+    '\\.(ts(x?)|js(x?))$': 'ts-jest',
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   testPathIgnorePatterns: ['/node_modules/', '/public/'],
-  collectCoverage: true,
   coverageReporters: ['lcov'],
   coverageDirectory: 'coverage',
+  collectCoverageFrom: [
+    'src/**/*.{js,jsx,ts,tsx}',
+    '!src/**/*.test.{js,jsx,ts,tsx}',
+    '!src/app.tsx',
+    '!src/styles/*.{ts,js}',
+  ],
   coverageThreshold: {
     global: {
-      branches: 80,
-      functions: 80,
-      lines: 80,
-      statements: 80,
+      statements: 98,
+      branches: 91,
+      functions: 98,
+      lines: 98,
     },
   },
+  globals: {
+    'ts-jest': {
+      tsConfig: 'tsconfig.json',
+    },
+  },
+  snapshotSerializers: [],
+  watchPlugins: [
+    'jest-watch-typeahead/filename',
+    'jest-watch-typeahead/testname',
+  ],
 };
